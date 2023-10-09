a9d7d167de00cc5029a05b4bc743c27927bf34ef
bhrug <bhruguni@ualberta.ca>
2023-10-09 00:14:10 -0600

    Adding docker-compose file and aws api gateway

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -7,6 +7,8 @@ provider "aws" {
   skip_requesting_account_id  = true
   endpoints {
     dynamodb = "http://localhost:4566"
+    apigateway = "http://localhost:4566"
+    lambda = "http://localhost:4574"
   }
 }
 
@@ -20,4 +22,22 @@ resource "aws_dynamodb_table" "example" {
     name = "ID"
     type = "N"
   }
+}
+
+resource "aws_api_gateway_rest_api" "gateway" {
+  name        = "gateway"
+  description = "This is my API for demonstration purposes"
+}
+
+resource "aws_api_gateway_resource" "gatewayResource" {
+  rest_api_id = aws_api_gateway_rest_api.gateway.id
+  parent_id   = aws_api_gateway_rest_api.gateway.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "gatewayMethod" {
+  rest_api_id   = aws_api_gateway_rest_api.gateway.id
+  resource_id   = aws_api_gateway_resource.gatewayResource.id
+  http_method   = "POST"
+  authorization = "NONE"
 }
